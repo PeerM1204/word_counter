97e6a43696434e3471f85ceb48351e4ea398526f
PeerM1204 <159874446+PeerM1204@users.noreply.github.com>
2024-04-25 23:47:31 +0500

    update the version

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
     {
         name: 'sentence',
         type: 'input',
-        message: 'Enter Your Sentence to count the Words'
+        message: 'Enter Your Sentence to count the Words:'
     }
 ]);
 const Words = answer.sentence.trim().split(" ");
+//print the array of words:
 console.log(Words);
-console.log(`Your Sentence word count is: ${Words.length}`);
+//print the words count:
+console.log(`Your Sentence words count is: ${Words.length}`);
